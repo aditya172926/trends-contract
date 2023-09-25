49259cb7f0be989bff6ca3f5604560befec863f4
Aditya <aditya26sg@gmail.com>
2023-09-26 00:56:34 +0530

    added modifiers and mint

--- contracts/DataDao.sol
+++ contracts/DataDao.sol
@@ -1,19 +1,37 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract DataDao {
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract DataDao is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private currentTokenId;
+
     string public dao_name;
     address public dao_owner;
+    string public baseTokenUri;
+    ERC721 dao_nft;
 
-    constructor (string memory name, address owner) {
+    constructor (string memory name, address owner, string memory dao_nft_symbol) ERC721(name, dao_nft_symbol) {
         dao_name = name;
         dao_owner = owner;
+        baseTokenUri = "";
+        dao_nft = ERC721(address(this));
     }
 
+    modifier checkMember(address member_address) {
+        uint tokenCount = dao_nft.balanceOf(member_address);
+        require (tokenCount == 0, "Cannot claim DAO NFT twice");
+        _;
+    }
 
-
-    function addMember(address member_address) public {
+    function addMember() public checkMember(msg.sender) returns (uint256) {
         // mint the nft to this address
+        currentTokenId.increment();
+        uint256 tokenId = currentTokenId.current();
+        _safeMint(msg.sender, tokenId);
+        return tokenId;
     }
 
     function removeMember(address member_address) public {
@@ -21,5 +39,11 @@ contract DataDao {
         // burn the nft for the member_address
     }
 
-    
+    function setBaseTokenUri(string memory _baseTokenUri) public {
+        baseTokenUri = _baseTokenUri;
+    }
+
+    function _baseUri() public view returns (string memory) {
+        return baseTokenUri;
+    }
 }
